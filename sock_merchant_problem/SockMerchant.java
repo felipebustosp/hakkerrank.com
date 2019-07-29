d9ef67e6be8d8e609a6a396c2fcc62019dd69c12
felipebustosp <felipe.bustosp@gmail.com>
2019-07-29 03:04:46 -0400

    problem 1

---
+++ sock_merchant_problem/SockMerchant.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+
+public class SockMerchant {
+	private Integer totalSocks;
+       	private ArrayList<Integer> sockList;
+
+	public SockMerchant(final Integer totalSocks, final ArrayList sockList){
+		this.totalSocks = totalSocks;
+		this.sockList = sockList;
+	}	
+
+}
+
